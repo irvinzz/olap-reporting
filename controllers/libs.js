da3147736951a78499532061f99e745c6123afd6
irvin <irvinzz@yandex.ru>
2013-01-17 09:19:09 +0600

    home run

--- controllers/libs.js
+++ controllers/libs.js
@@ -145,20 +145,20 @@ serverClass.implements({
         });
     },
     logout: function(p,c){
-		var _this = this;
+        var _this = this;
         this._sR(p,{f: 'logout',k: ['OK']},function(err,result){
-			if (!err){
-				_this.palo.sid = null;
-				_this.palo.ttl = null;
-			}
-			c(err,result);
-		});
+            if (!err){
+                _this.palo.sid = null;
+                _this.palo.ttl = null;
+            }
+            c(err,result);
+        });
     },
     save: function(p,c){
         this._sR(p,{f:'save',k:['OK']},c);
     },
     shutdown: function(p,c){
-        
+        this._sR(p,{f:'shutdows',k:['OK']},c);
     }
 });
 
@@ -167,8 +167,29 @@ var databaseClass = Class("DatabaseClass",{
 });
 databaseClass.extends(paloSubClass,false);
 databaseClass.implements({
-    cubes: function(){
-            
+    sr: function(params,diff,callback){
+        this.palo.hg(
+            '/database/'+diff.f,
+            params,
+            diff.k,
+            callback
+        );
+    },
+    /*
+     * 
+     * 
+     * 0	cube	identifier	Identifier of the cube
+1	name_cube	string	Name of the cube
+2	number_dimensions	integer	Number of dimensions
+3	dimensions	integer	Comma separate list of dimension identifiers
+4	number_cells	integer	Total number of cells
+5	number_filled_cells	integer	Number of filled numeric base cells plus number of filled string cells
+6	status	integer	Status of cube (0=unloaded, 1=loaded and 2=changed)
+7	type	integer	Type of cube (0=normal, 1=system, 2=attribute, 3=user info, 4=gpu type)
+8	cube_token
+     */
+    cubes: function(p,c){
+        this.sr(p,{f:'cubes',k:['cube','name_cube','number_dimensions','dimensions','number_cells','number_filled_cells','status','type','cube_token']},c)
     },
     create: function(){
         
@@ -176,9 +197,28 @@ databaseClass.implements({
     destroy: function(){
         
     },
-    dimensions: function(){
-        
+    dimensions: function(p,c){
+        this.sr(
+            p,
+            {
+                f: 'dimensions',
+                k:[
+                    'name_dimension',   //  string	Name of the dimension'
+                    'number_elements',  //  integer	Number of elements
+                    'maximum_level',    //  integer	Maximum level of the dimension
+                    'maximum_indent',   //  integer	Maximum indent of the dimension
+                    'maximum_depth',    //  integer	Maximum depth of the dimension
+                    'type',             //  integer	Type of dimension (0=normal, 1=system, 2=attribute, 3=user info)
+                    'attributes_dimension', //  identifier	Identifier of the attributes dimension of a normal dimension or the identifier of the normal dimension associated to a attributes dimension.
+                    'attributes_cube',  //  identifier	Identifier of the attributes cube. (only for normal dimensions)
+                    'rights_cube',      //  identifier	Identifier of the rights cube. (only for normal dimensions)
+                    'dimension_token',  //  integer	The dimension token of the dimension
+                ]
+            },
+            c
+        )
     },
+
     info: function(){
         
     },
